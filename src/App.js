15d1b7b9fe1589f8a7c56fc7e464892b9a44f156
zakariaelb <zakari_a@icloud.com>
2023-05-10 21:36:21 +0400

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
+import React from "react";
+import './App.css';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
 
 export default function App() {
     return (
